b8f3758aa0a1fe91d205615462e3f1190457cb65
amsmyslov <a.m.smyslov@yandex.ru>
2024-03-18 18:24:44 +0300

    Initial commit

---
+++ src/main/java/org/example/service/logic/RegisterLogic.java
@@ -0,0 +1,32 @@
+package org.example.service.logic;
+
+import org.example.statemachine.TransmittedData;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+public class RegisterLogic {
+
+    public SendMessage processWaitingCommandStart(String textFromUser, TransmittedData transmittedData) throws Exception {
+
+        return null;
+    }
+
+    public SendMessage processWaitingInputTeamName(String textFromUser, TransmittedData transmittedData) throws Exception {
+
+        return null;
+    }
+
+    public SendMessage processWaitingInputNumberOfParticipants(String textFromUser, TransmittedData transmittedData) throws Exception {
+
+        return null;
+    }
+
+    public SendMessage processWaitingInputSelectedTask(String textFromUser, TransmittedData transmittedData) throws Exception {
+
+        return null;
+    }
+
+    public SendMessage processWaitingApproveData(String textFromUser, TransmittedData transmittedData) throws Exception {
+
+        return null;
+    }
+}
